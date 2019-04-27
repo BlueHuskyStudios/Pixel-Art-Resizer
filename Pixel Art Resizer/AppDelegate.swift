232da73dc2755d1ca270d75d53fc8fdfed993e85
Ben Leggiero <BenLeggiero@Gmail.com>
2019-04-26 20:00:20 -0400

    Initial Commit

---
+++ Pixel Art Resizer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Pixel Art Resizer
+//
+//  Created by Ben Leggiero on 4/26/19.
+//  Copyright © 2019 Ben Leggiero. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
