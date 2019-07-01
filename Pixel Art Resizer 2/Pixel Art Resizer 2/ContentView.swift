915cd9c4e42ccf1e523645dcad3b082f9d95d055
Ben Leggiero <BenLeggiero@Gmail.com>
2019-07-01 16:33:54 -0600

    WIP: Starting with a new project

---
+++ Pixel Art Resizer 2/Pixel Art Resizer 2/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Pixel Art Resizer 2
+//
+//  Created by Ben Leggiero on 6/16/19.
+//  Copyright © 2019 Ben Leggiero. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
